5be21dbe81e3d4d48d0041f98affba52fbb8abde
Joao Stein <joaosteing@gmail.com>
2016-03-27 09:35:51 -0300

    add required argument

--- app/index.js
+++ app/index.js
@@ -1,6 +1,7 @@
 'use strict';
 
 var generators = require('yeoman-generator');
+var _ = require('lodash');
 
 module.exports = generators.Base.extend({
   // The name `constructor` is important here
@@ -10,22 +11,28 @@ module.exports = generators.Base.extend({
 
     // Next, add your custom code
     this.option('coffee'); // This method adds support for a `--coffee` flag
+
+    // This makes `appname` a required argument.
+    this.argument('appname', { type: String, required: true });
+    // And you can then access it later on this way; e.g. CamelCased
+    this.appname = _.camelCase(this.appname);
   },
 
   prompting: function () {
-    var done = this.async();
-    this.prompt({
-      type    : 'input',
-      name    : 'name',
-      message : 'Your project name',
-      default : 'toyproblem-scaffold'
-    }, function (answers) {
-      this.log(answers.name);
-      done();
-    }.bind(this));
+    // var done = this.async();
+    // this.prompt({
+    //   type    : 'input',
+    //   name    : 'name',
+    //   message : 'Your project name',
+    //   default : 'toyproblem-scaffold'
+    // }, function (answers) {
+    //   this.log(answers.name);
+    //   done();
+    // }.bind(this));
   },
 
   method1: function () {
+    console.log(this.appname);
     console.log('method 1 just ran');
   },
 
